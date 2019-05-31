0943a463e1acf2b8d52ffddbb0638d1f01e4e59f
Stephen Jeong <stephen.h.jeong@gmail.com>
2019-05-30 17:58:14 -0700

    Refactor VideoListEntry to be stateless functional component rather than dynamic

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,22 +1,15 @@
-class VideoListEntry extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="video-list-entry media">
-      <div className="media-left media-middle">
-        <img className="media-object" src={this.props.video.snippet.thumbnails.default.url} alt={this.props.video.snippet.title} />
-      </div>
-      <div className="media-body">
-        <div className="video-list-entry-title">{this.props.video.snippet.title}</div>
-        <div className="video-list-entry-detail">{this.props.video.snippet.description}</div>
-      </div>
+var VideoListEntry = props => (
+  <div className="video-list-entry media">
+    <div className="media-left media-middle">
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt={props.video.snippet.title} />
     </div>
-    );
-  }
-}
+    <div className="media-body">
+      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+      <div className="video-list-entry-detail">{props.video.snippet.description}</div>
+    </div>
+  </div>
+);
+
 
 
 
